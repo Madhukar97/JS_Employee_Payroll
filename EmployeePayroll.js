04934c30d2b6c5ae3106dc85bc40639bf447ebac
unknown <madhukar.tatiraju@gmail.com>
2021-12-06 01:38:39 +0530

    Added: Ability to store daily wage in an array

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -5,20 +5,31 @@ const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HR = 20;
 const WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+let empDailyWageArray = new Array();
+let totalWage = 0;
 
-function calcWagesForAMonth(WORKING_DAYS) {
-  let empHrs = 0;
+function calcWagesForAMonth() {
+  let totalEmpHrs = 0;
   let days=0;
-  while(days<WORKING_DAYS && empHrs<=160){
+
+  while(days<WORKING_DAYS && totalEmpHrs<=MAX_HRS_IN_MONTH){
     let empType = Math.floor(Math.random() * 3);
-    empHrs+=getWorkingHrs(empType);
+    totalEmpHrs+=getWorkingHrs(empType)
+    empDailyWageArray.push(calculateWage(getWorkingHrs(empType)));
     days++;
   }
-  let empWageForMonth = empHrs * WAGE_PER_HR;
+
+  let empWageForMonth = totalEmpHrs * WAGE_PER_HR;
   console.log(`    Total working days = ${days} 
-    Total Hrs = ${empHrs }
-    Total Wage for Month = ${empWageForMonth}`)
+    Total Hrs = ${totalEmpHrs}
+    Total Wage for Month = ${empDailyWageArray.reduce((totalWage, dailyWage) => totalWage + dailyWage)}`)
+
   return empWageForMonth;
+}
+
+function calculateWage(empHrs){
+  return WAGE_PER_HR * empHrs
 }
 
 function getWorkingHrs(empType) {
@@ -29,4 +40,4 @@ function getWorkingHrs(empType) {
   }
 }
 
-calcWagesForAMonth(WORKING_DAYS);
+calcWagesForAMonth();
