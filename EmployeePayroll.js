caa79248f8414d27d61f3b381bb6aa4d531e7dc2
unknown <madhukar.tatiraju@gmail.com>
2021-12-06 02:14:41 +0530

    Added: Array Helper Functions

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -23,7 +23,7 @@ function calcWagesForAMonth() {
   let empWageForMonth = totalEmpHrs * WAGE_PER_HR;
   console.log(`    Total working days = ${days} 
     Total Hrs = ${totalEmpHrs}
-    Total Wage for Month = ${empDailyWageArray.reduce((totalWage, dailyWage) => totalWage + dailyWage)}`)
+    Total Wage for Month = ${empDailyWageArray.reduce((totalWage, dailyWage) => totalWage + dailyWage)}`)   //UC-7a Calc total Wage using reduce mehod
 
   return empWageForMonth;
 }
@@ -41,3 +41,28 @@ function getWorkingHrs(empType) {
 }
 
 calcWagesForAMonth();
+
+//UC-7b show day along with daily wage using array map function
+let day = 1;
+let arrMap = empDailyWageArray.map(dailyWage => 'Day ' + day++ + ' Wage = ' + dailyWage);
+console.log(arrMap);
+
+
+//UC-7c Show Days when Full time wage of 160 were earned using filter function
+let fullTimeWageArray = arrMap.filter(dailyWage => dailyWage.includes(160));
+console.log('Days with full time wage: ')
+console.log(fullTimeWageArray);
+
+//UC-7d Find the first occurrence when Full Time Wage was earned using find function
+console.log('First occurance of full time wage is Day: ' + (empDailyWageArray.findIndex(d => d==160) + 1));
+
+//UC-7e Check if Every Element of Full Time Wage is truly holding Full time wage
+console.log(fullTimeWageArray.every(dw => dw.includes(160)));
+
+//UC-7f Check if any part time Wage
+console.log('Check is there any part time wage: ' + arrMap.some(dw => dw.includes(80)));
+
+//UC-7g Find number of days employee worked
+let workingDays = empDailyWageArray.filter(dw => dw>0);
+console.log("Number of days Employee worked = "+workingDays.length);
+
